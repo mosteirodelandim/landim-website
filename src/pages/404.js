9e925e37ef212a2a6cc2c08482b802e42facc641
1150625_LuisArteiro <1150625@isep.ipp.pt>
2019-10-08 11:07:42 +0100

    Adding 404 page and changing menu color.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,16 +1,34 @@
 import React from 'react';
 
-import Layout from '../components/Layout';
+import Lottie from 'lottie-react-web';
+import dogWagging from '../assets/lottie/dog_wagging';
+
+function randomDog() {
+  const dogArray = ['Ringo', 'Spot'];
+
+  const rand = Math.floor(Math.random() * dogArray.length);
+  return dogArray[rand];
+}
 
 const IndexPage = () => (
-  <Layout fullMenu>
-    <article id="main">
-      <header>
-        <h2>Not Found</h2>
-        <p>Not a Valid URL</p>
-      </header>
-    </article>
-  </Layout>
+  <div id="not-found-page">
+    <div className="error-wall load-error">
+      <div className="error-container">
+        <div className="h1_notfound">oh no...</div>
+        <div className="h3_notfound">You got a 404 error</div>
+        <div className="p_notfound">You should probably head <a onClick={() => history.back()}>back</a> to Landim, you won&apos;t find anything here
+          (except perhaps {randomDog()}, the house dog).
+        </div>
+      </div>
+      <Lottie
+        options={{
+          animationData: dogWagging,
+        }}
+        width="50%"
+        height="50%"
+      />
+    </div>
+  </div>
 );
 
 export default IndexPage;
