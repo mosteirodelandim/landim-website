9c36a465a6d0b71fb736fc0563bfd5146f609771
Luis Arteiro [1150625] <1150625@isep.ipp.pt>
2019-10-12 02:34:00 +0100

    Removing suspense priority

--- src/i18n.js
+++ src/i18n.js
@@ -1,14 +1,14 @@
 import i18n from 'i18next';
 import Backend from 'i18next-xhr-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import { reactI18nextModule } from 'react-i18next';
+import { initReactI18next } from 'react-i18next';
 
 i18n
   .use(Backend)
   .use(LanguageDetector)
-  .use(reactI18nextModule)
+  .use(initReactI18next)
   .init({
-    fallbackLng: 'en',
+    fallbackLng: 'en-GB',
 
     // have a common namespace used around the full app
     ns: ['translations'],
@@ -22,6 +22,7 @@ i18n
 
     react: {
       wait: true,
+      useSuspense: false,
     },
   });
 
