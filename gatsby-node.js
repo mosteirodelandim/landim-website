6b96d0781a70d858c30135165d1a40a807683ef7
Luis Arteiro [1150625] <1150625@isep.ipp.pt>
2019-10-12 01:39:23 +0100

    Adding locales directory, i18n component and hooking gatsby-node.

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,13 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+const fs = require('fs-extra');
+const path = require('path');
+
+exports.onPostBuild = () => {
+  console.log('Copying locales');
+  fs.copySync(
+    path.join(__dirname, '/src/locales'),
+    path.join(__dirname, '/public/locales'),
+  );
+};
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,6 @@
 import React from 'react';
+// TODO see if I need to import dedicated sass file
+// import '../assets/sass/layout/_notfound.scss';
 
 import Lottie from 'lottie-react-web';
 import dogWagging from '../assets/lottie/dog_wagging';
