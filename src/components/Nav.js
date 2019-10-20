e96020ab3eb43965b053484f9e5be835243af3be
Luis Arteiro [1150625] <1150625@isep.ipp.pt>
2019-10-20 23:01:46 +0100

    Adding toggle to open up History submenus on Menu.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
+import { SlideDown } from 'react-slidedown';
 import FlagIcon from './FlagIcon';
 import i18n from '../i18n/i18n';
+import 'react-slidedown/lib/slidedown.css';
 
+function historyTab(t, toggleHistory, setToggleHistory) {
+  return (
+    <>
+      <li style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Link to="/history">{t('sidebar:history.main')}</Link>
+        <a onClick={() => setToggleHistory(!toggleHistory)}>
+          { toggleHistory
+            ? <span className="icon solid fa-chevron-down" />
+            : <span className="icon solid fa-chevron-left" />}
+        </a>
+      </li>
+
+      <SlideDown className="my-dropdown-slidedown">
+        {toggleHistory
+
+          ? (
+            <>
+              <li id="li-submenu">
+                <Link to="/history#origin">{t('sidebar:history.origins')}</Link>
+              </li>
+              <li id="li-submenu">
+                <Link to="/history#priests">{t('sidebar:history.priests')}</Link>
+              </li>
+              <li id="li-submenu">
+                <Link to="/history#extinction">{t('sidebar:history.extinction')}</Link>
+              </li>
+              <li id="li-submenu">
+                <Link to="/history#sale">{t('sidebar:history.sale')}</Link>
+              </li>
+              <li id="li-submenu">
+                <Link to="/history#notable">{t('sidebar:history.notable')}</Link>
+              </li>
+              <li id="li-submenu">
+                <Link to="/history#book">{t('sidebar:history.book')}</Link>
+              </li>
+            </>
+          ) : null}
+      </SlideDown>
+    </>
+  );
+}
 
 function Nav({ onMenuToggle = () => {} }) {
   const { t } = useTranslation();
+  const [toggleHistory, setToggleHistory] = useState(false);
+
   return (
     <nav id="nav">
       <ul>
@@ -46,36 +91,21 @@ function Nav({ onMenuToggle = () => {} }) {
               <li>
                 <Link to="/">{t('sidebar:landing')}</Link>
               </li>
-              <li>
-                <Link to="/history">{t('sidebar:history.main')}</Link>
-              </li>
-              <li id="li-submenu">
-                <Link to="/history#origin">{t('sidebar:history.origins')}</Link>
-              </li>
-              <li id="li-submenu">
-                <Link to="/history#priests">{t('sidebar:history.priests')}</Link>
-              </li>
-              <li id="li-submenu">
-                <Link to="/history#extinction">{t('sidebar:history.extinction')}</Link>
-              </li>
-              <li id="li-submenu">
-                <Link to="/history#sale">{t('sidebar:history.sale')}</Link>
-              </li>
-              <li id="li-submenu">
-                <Link to="/history#notable">{t('sidebar:history.notable')}</Link>
-              </li>
-              <li id="li-submenu">
-                <Link to="/history#book">{t('sidebar:history.book')}</Link>
-              </li>
+
+              {historyTab(t, toggleHistory, setToggleHistory)}
+
               <li>
                 <Link to="/services">{t('sidebar:services')}</Link>
               </li>
+
               <li>
                 <Link to="/services#photogallery">{t('sidebar:photos')}</Link>
               </li>
+
               <li>
                 <Link to="/wine_stay">{t('sidebar:wine_stay')}</Link>
               </li>
+
               <li>
                 <Link to="/faq">{t('sidebar:faq')}</Link>
               </li>
