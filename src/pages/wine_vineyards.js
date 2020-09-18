2b0340a1a01e15e6aa0debc7dbde5e819a090813
Luis Arteiro <lucho14garfield@gmail.com>
2020-09-18 01:49:42 +0100

    :sparkles: Adding lightbox to Vine and Vineyards. #21

--- src/pages/wine_vineyards.js
+++ src/pages/wine_vineyards.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import Lottie from 'lottie-react-web';
 import LandingLayout from '../components/LandingLayout';
+import Lightbox from 'react-image-lightbox';
+import 'react-image-lightbox/style.css';
 
 import winePic1 from '../assets/images/wine_vineyards/wine1.jpg';
 import lottieAnim from '../assets/images/lottie/sleeping';
 import config from '../../config';
 import Obfuscate from 'react-obfuscate';
+import bookPic1 from '../assets/images/history/book/book1.jpg';
+
+const images = [winePic1]
 
 function WineStayPage() {
   const { t } = useTranslation();
+  const [lightboxIsOpen, setToggleLightbox] = useState(false);
+  const [photoIndex, setPhotoIndex] = useState(0);
+
+  const captions = [t('wine_vineyards:imageCaptions.wine')];
 
   return (
     <LandingLayout fullMenu>
@@ -24,6 +33,10 @@ function WineStayPage() {
 
             <>
               <span
+                onClick={() => {
+                  setPhotoIndex(images.indexOf(winePic1));
+                  setToggleLightbox(true);
+                }}
                 className="image right"
                 style={{
                   width: '80vh', fontSize: '0.8em', color: 'gray', textAlign: 'right',
@@ -66,6 +79,18 @@ function WineStayPage() {
         </section>
 
       </article>
+
+      {lightboxIsOpen && (
+        <Lightbox
+          mainSrc={images[photoIndex]}
+          nextSrc={images[(photoIndex + 1) % images.length]}
+          prevSrc={images[(photoIndex + images.length - 1) % images.length]}
+          onCloseRequest={() => setToggleLightbox(false)}
+          onMovePrevRequest={() => setPhotoIndex((photoIndex + images.length - 1) % images.length)}
+          onMoveNextRequest={() => setPhotoIndex((photoIndex + 1) % images.length)}
+          imageCaption={`(${photoIndex + 1}/${images.length}) ${captions[photoIndex]}`}
+        />
+      )}
     </LandingLayout>
   );
 }
