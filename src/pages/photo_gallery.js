3594d607d182bd2f7f895e6537246a0d5c6e5753
Luis <lucho14garfield@gmail.com>
2021-02-21 19:21:59 +0000

    :speech_balloon: :fire: Cleaning code and text on Photo Gallery

--- src/pages/photo_gallery.js
+++ src/pages/photo_gallery.js
@@ -3,20 +3,6 @@ import { useTranslation } from 'react-i18next';
 import LandingLayout from '../components/LandingLayout';
 import 'react-image-lightbox/style.css';
 
-import Carousel from 're-carousel';
-import IndicatorDots from '../components/CarouselDots';
-
-import church_cloister1 from '../assets/images/photo-gallery/cloister_church/church_cloister1.jpg';
-import church_cloister2 from '../assets/images/photo-gallery/cloister_church/church_cloister2.jpg';
-import church_cloister3 from '../assets/images/photo-gallery/cloister_church/church_cloister3.jpg';
-import church_cloister4 from '../assets/images/photo-gallery/cloister_church/church_cloister4.jpg';
-import church_cloister5 from '../assets/images/photo-gallery/cloister_church/church_cloister5.jpg';
-
-import gardens_forest1 from '../assets/images/photo-gallery/gardens_forest/gardens_forest1.jpg';
-import gardens_forest2 from '../assets/images/photo-gallery/gardens_forest/gardens_forest2.jpg';
-import gardens_forest3 from '../assets/images/photo-gallery/gardens_forest/gardens_forest3.png';
-import gardens_forest4 from '../assets/images/photo-gallery/gardens_forest/gardens_forest4.jpg';
-import gardens_forest5 from '../assets/images/photo-gallery/gardens_forest/gardens_forest5.jpg';
 import PhotoGallery from '../components/Photogallery';
 
 function PhotoGalleryPage() {
