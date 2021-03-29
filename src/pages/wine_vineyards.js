21e0d021a538d36a8c2e6ebac763ffbad5feb320
Lucho <lucho14garfield@gmail.com>
2021-03-29 21:39:49 +0100

    :sparkles: Refining Wine & Vineyards page.

--- src/pages/wine_vineyards.js
+++ src/pages/wine_vineyards.js
@@ -2,19 +2,14 @@ import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import Lightbox from 'react-image-lightbox';
 import { Col, Grid, Row } from 'react-flexbox-grid';
-import { Link } from 'gatsby';
-import Obfuscate from 'react-obfuscate';
 import LandingLayout from '../components/LandingLayout';
 import 'react-image-lightbox/style.css';
 
 import winePic1 from '../assets/images/wine_vineyards/wine1.jpg';
-import lottieAnim from '../assets/images/lottie/sleeping';
-import config from '../../config';
-import bookPic1 from '../assets/images/history/book/book1.jpg';
-import spaces1 from '../assets/images/about_us/spaces1.jpg';
-import spaces2 from '../assets/images/about_us/spaces2.jpg';
-import spaces3 from '../assets/images/about_us/spaces3.jpg';
-import spaces4 from '../assets/images/about_us/spaces4.png';
+
+import spaces1 from '../assets/images/wine_vineyards/feature_1.jpg';
+import spaces2 from '../assets/images/wine_vineyards/feature_2.jpg';
+import spaces3 from '../assets/images/wine_vineyards/feature_3.jpg';
 
 const images = [winePic1];
 
@@ -27,7 +22,7 @@ function WineStayPage() {
 
   return (
     <LandingLayout fullMenu>
-      <article id="pageMain">
+      <article id="wineMain">
         <header id="winestayHeader">
           <h2>{t('wine_vineyards:title')}</h2>
           <p>{t('wine_vineyards:subHeading')}</p>
@@ -65,10 +60,54 @@ function WineStayPage() {
           </div>
         </section>
 
-        {
-          //TODO fazer secção com as três imagens lado a lado e depois um slider de imagens em baixo.
-        }
+        <section className="wrapper style5">
+          <div className="inner">
+            <header>
+              <h3>{t('wine_vineyards:vines.title')}</h3>
+            </header>
 
+            <p>{t('wine_vineyards:vines.description1')}</p>
+          </div>
+
+          <Grid>
+            <Row>
+              <Col lg={4} md={6}>
+                <span className="image fit hover-container">
+                  <img src={spaces1} alt="" />
+                  <div className="overlay">
+                    <div className="text">
+                      <h2>{t('wine_vineyards:images.image1.title')}</h2>
+                      <span>{t('wine_vineyards:images.image1.subtitle')}</span>
+                    </div>
+                  </div>
+                </span>
+              </Col>
+              <Col lg={4} md={6}>
+                <span className="image fit hover-container">
+                  <img src={spaces2} alt="" />
+                  <div className="overlay">
+                    <div className="text">
+                      <h2>{t('wine_vineyards:images.image2.title')}</h2>
+                      <span>{t('wine_vineyards:images.image2.subtitle')}</span>
+                    </div>
+                  </div>
+                </span>
+              </Col>
+              <Col lg={4} md={6}>
+                <span className="image fit hover-container">
+                  <img src={spaces3} alt="" />
+                  <div className="overlay">
+                    <div className="text">
+                      <h2>{t('wine_vineyards:images.image3.title')}</h2>
+                      <span>{t('wine_vineyards:images.image3.subtitle')}</span>
+                    </div>
+                  </div>
+                </span>
+              </Col>
+            </Row>
+          </Grid>
+        </section>
+        
       </article>
 
       {lightboxIsOpen && (
