45552bd8a5b1829e601bdaa2a6d391e9d50226d1
LuchoTurtle <lucho14garfield@gmail.com>
2023-07-29 13:25:39 +0100

    chore: Remove source files.

--- src/components/FlagIcon.js
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-import FlagIconFactory from 'react-flag-icon-css';
-
-// const FlagIcon = FlagIconFactory(React);
-const FlagIcon = FlagIconFactory(React, { useCssModules: false });
-
-export default FlagIcon;
